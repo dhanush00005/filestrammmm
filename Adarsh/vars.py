cea8cf07d24cccb174c768f19acd7017759fb897
dhanush00005 <122075054+dhanush00005@users.noreply.github.com>
2023-10-28 13:03:19 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -11,11 +11,11 @@ class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv('API_ID', "28313782"))
     API_HASH = str(getenv('API_HASH', "52e8687988e1be742f5f73ea392e89bc"))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', "6519574916:AAG3POKqQZaK9A__GaW-jOVdNfBylJ6X47E"))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', "6616860068:AAF924Q5JX2a9Qz4J5LndEPj0IppwL8ckpI"))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', "-1001968895813"))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', "-1001991965904"))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
@@ -25,7 +25,7 @@ class Var(object):
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', "Dhanush1300"))
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME', "tmplinjkkk-8ab94b48362d"))
+        APP_NAME = str(getenv('APP_NAME', "filetolinkkk-d00aadaa35b6"))
     
     else:
         ON_HEROKU = False
@@ -35,6 +35,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', "mongodb+srv://DhabusBot:dhanusbot@dhanusbot.z2rec8x.mongodb.net/?retryWrites=true&w=majority"))
+    DATABASE_URL = str(getenv('DATABASE_URL', "mongodb+srv://Tyson143:Tyson143@cluster0.ut3vhhd.mongodb.net/?retryWrites=true&w=majority"))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
